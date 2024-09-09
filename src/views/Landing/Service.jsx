c32a474b1d1332168f3161cb84b7824c7246088a
Muhammad Moaz <158042944+moazsaeedantechpvt@users.noreply.github.com>
2024-09-09 18:36:59 +0500

    landing page faqs

--- src/views/Landing/Service.jsx
+++ src/views/Landing/Service.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ServiceHomeCard from '../../components/ServiceHomeCard';
-import { MdWeb, MdSecurity, MdSpeed } from 'react-icons/md'; // Example icons from react-icons
+import { MdWeb, MdSecurity, MdSpeed } from 'react-icons/md';
+import Button from '../../components/Button';
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const servicesData = [
     {
@@ -18,39 +20,82 @@ const servicesData = [
         icon: <MdSecurity />,
         mainHeading: 'Custom Software Development',
         subHeadings: [
-            "Data Encryption",
-            "Network Security",
-            "Threat Detection & Response"
+            "APIS",
+            " Enterprise  ",
+            "Micro Services",
+            "All Powered Solutions",
+            "CRM",
         ]
     },
     {
-        icon: <MdSpeed />, // Icon for performance optimization
-        mainHeading: 'Performance Optimization',
+        icon: <MdSpeed />,
+        mainHeading: 'Mobile App Development',
         subHeadings: [
-            "Speed Enhancements",
-            "Code Optimization",
-            "Server Performance Tuning"
+            "Android App",
+            "IOS",
+            "Hybrid Apps",
+            "Flutter Apps",
+            "Social Apps",
         ]
-    }
+    },
+    {
+        icon: <MdSpeed />,
+        mainHeading: 'CMS Development',
+        subHeadings: [
+            "WordPress",
+            "Shopify",
+            "Wix",
+            "Web Flow",
+            "Custom CMS",
+        ]
+    },
+    {
+        icon: <MdSpeed />,
+        mainHeading: 'UIUX Designing',
+        subHeadings: [
+            "Website Design",
+            "Mobile App Development",
+            "Usability Testing",
+            "Wireframing",
+        ]
+    },
+    {
+        icon: <MdSpeed />,
+        mainHeading: 'Digital Marketing',
+        subHeadings: [
+            "SEO",
+            "PPC",
+            "SMM",
+            "Social Media Management",
+            "Brand Kits",
+        ]
+    },
 ];
 
 const Service = () => {
     return (
         <>
-            <div className='flex flex-col items-center gap-4'>
-                <h3 className='text-4xl font-bold text-primary'>Services We Deliver</h3>
-                <p className='text-2xl font-thin'>Our Featured Services</p>
-            </div>
-            <div className='grid grid-cols-12 gap-6 py-12'>
-                {servicesData.map((service, index) => (
-                    <div key={index} className='col-span-4'>
-                        <ServiceHomeCard
-                            icon={service.icon}
-                            mainHeading={service.mainHeading}
-                            subHeadings={service.subHeadings}
-                        />
-                    </div>
-                ))}
+            <div className='flex flex-col items-center'>
+
+                <div className='flex flex-col items-center gap-4'>
+                    <h3 className='text-4xl font-bold text-primary'>Services We Deliver</h3>
+                    <p className='text-2xl font-thin'>Our Featured Services</p>
+                </div>
+                <div className='grid grid-cols-12 gap-8 py-12'>
+                    {servicesData.map((service, index) => (
+                        <div key={index} className='col-span-4'>
+                            <ServiceHomeCard
+                                icon={service.icon}
+                                mainHeading={service.mainHeading}
+                                subHeadings={service.subHeadings}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <div>
+
+                    <Button icon={<FaArrowRightLong />}>Book my Consultation Today</Button>
+                </div>
             </div>
         </>
     );
